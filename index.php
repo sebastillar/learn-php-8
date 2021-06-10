f434c5c1329b2b6f05881ea7194c3d10d0b1fe0e
SebastianVillar <sebastian.villar@outlook.com>
2021-06-10 06:08:57 -0300

    Match Expressions

--- index.php
+++ index.php
@@ -15,3 +15,13 @@ class Profile {
 //Nullsafe Operator ?
 $user = new User;
 echo $user->profile()?->employment() ?? 'Unemployed'; 
+
+//Match Expressions
+$obj = new User();
+$type = match (get_class($obj)){
+    'User' => 'is_a_user',
+    'Profile' => 'has_a_profile',
+    'Cat' => 'It´s a lovely meow animal'
+};
+
+echo $type;
